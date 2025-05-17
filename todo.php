d0b6e8678850e4b1415f50b5e869d33d08fe9eb8
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-18 03:30:00 +0600

    Add Task done

--- todo.php
+++ todo.php
@@ -1,3 +1,14 @@
 <?php
 // Load tasks
 $tasks = file_exists('tasks.json') ? json_decode(file_get_contents('tasks.json'), true) : [];
+
+// Handle Add Task
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_task'])) {
+    $newTask = trim($_POST['new_task']);
+    if ($newTask !== '') {
+        $tasks[] = ['id' => time(), 'task' => $newTask, 'done' => false];
+        file_put_contents('tasks.json', json_encode($tasks));
+    }
+    header("Location: todo.php");
+    exit;
+}
